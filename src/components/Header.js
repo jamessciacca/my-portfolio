8705b5b6dd3f76ae4d83d0f475f0cede9057e04e
jamessciacca <noing2003@gmail.com>
2023-05-20 18:12:37 -0400

    created a basic header with styling!

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,25 @@
 //importing react 
 import React from "react";
+//importing image
 
 //creating header function
 function Header() {
     return (
-        <h1>Logo</h1>
+        <>
+        <section id="header" className="p-4">
+        <div className="flex justify-center justify-between content-center">
+            {/* Logo Image and Text */}
+            <h1 className="ml-2 text-xl">JS</h1>
+            {/* Nav Items */}
+            <ul className="flex">
+                <li><a href="">About Me</a></li>
+                {/* added spacing between links */}
+                <li className="ml-2 mr-2"><a href="">Projects</a></li>
+                <li className="mr-2"><a href="">Contact Me</a></li>
+            </ul>
+        </div>
+        </section>
+        </>
     );
 }
 
