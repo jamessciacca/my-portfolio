ec5efd729fccd35da148915c47764b040b27e5c5
jamessciacca <noing2003@gmail.com>
2023-05-20 22:31:55 -0400

    created a basic About.js file and imported it to App.js

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+//importing react
+import React from "react";
+
+//creating about function
+function About() {
+    return(
+        <>
+        <section>
+            <div>
+                <h1>About</h1>
+            </div>
+        </section>
+        </>
+    );
+}
+
+export default About;
