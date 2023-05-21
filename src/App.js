055cfeeca3176516dc52a6c78c127350b717049f
jamessciacca <noing2003@gmail.com>
2023-05-20 22:25:03 -0400

    created basic footer function and put it into app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 //component imports
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+//importing React
+import React from "react";
+
+//creating footer function
+function Footer() {
+    return(
+        <>
+        <section>
+            <div>
+                <h1>Footer</h1>
+            </div>
+        </section>
+        </>
+    );
+}
+
+export default Footer;
