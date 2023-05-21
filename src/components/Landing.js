2798dd32a3158679b62dcaf310b04bc553a5da6e
jamessciacca <noing2003@gmail.com>
2023-05-21 16:46:18 -0400

    created a skills page, attempted to make a graph but it is not looking great

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -17,10 +17,10 @@ function Landing() {
                 <h1 id='name' className="font-mono text-8xl mb-5 mt-5 mr-5">James Sciacca</h1>
                 <h1 id='fullStack' className="font-mono text-4xl mb-5">A Full Stack Web Developer!</h1>
                 <p id='bio' className="font-mono mb-2">I specialize in all aspects of web development from front-end coding to back-end database development. My core skills include coding in JavaScript, React, and HTML/CSS. I have a passion for creating innovative, secure, and reliable websites with amazing user experiences. My goal is to use my skills and experience to develop amazing web applications and websites!</p>
-                <ul id='socials' className="flex items-center">
-                    <li><button id='contactBtn' className="font-mono py-2 px-4 rounded mt-3">Contact Me</button></li>
-                    <li className="mt-2 ml-2"><a href=""><img src={githublogo} alt="" /></a></li>
-                    <li className="mt-2 ml-2"><a href=""><img src={linkedinlogo} alt="" /></a></li>
+                <ul id='socials' className="flex items-center mt-5">
+                    <li><a id='contactBtn' className="text-white font-semibold bg-primary hover:bg-primarytrans hover:text-indigo-600 rounded py-2 px-4 border-2 focus:outline-none focus:border-teal-500 transition duration-150 ease-in-out">Contact Me</a></li>
+                    <li className="ml-2"><a href=""><img src={githublogo} alt="" /></a></li>
+                    <li className= "ml-2"><a href=""><img src={linkedinlogo} alt="" /></a></li>
                 </ul>
             </div>
         </section>
