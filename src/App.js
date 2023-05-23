788afebfb40c2b697978f5e01c558dd8fbe82b07
jamessciacca <noing2003@gmail.com>
2023-05-22 20:36:33 -0400

    pushing to heroku

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <>
-      <Header />
+      <Header/>
       <Landing />
       <Skills />
       <Projects />
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 //creating skills function
 function Skills() {
-    return(
-        <> 
-        <section>
-            <h1></h1>
-        </section>
+    return (
+        <>
+            <section>
+                <div className="row">
+                    <img src="image1.jpg" alt="image1" />
+                    <img src="image2.jpg" alt="image2" />
+                    <img src="image3.jpg" alt="image3" />
+                    <img src="image4.jpg" alt="image4" />
+                </div>
+            </section>
         </>
     );
 
