d9a73e5cecef33494db8836fbd7efd3f4bfecfb7
jamessciacca <noing2003@gmail.com>
2023-06-12 13:45:02 -0400

    completed page

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
-        <Route path='/MoreAbout' element={<MoreAbout />} />
+        <Route path='/MoreAboutMe' element={<MoreAbout />} />
       </Routes>
     </Router>
     </>
--- src/components/MoreAbout.js
+++ src/components/MoreAbout.js
@@ -3,24 +3,27 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 
-import selfPortrait from "../img/portraitpic.PNG";
+import selfPortrait from "../img/portraitpic.jpeg";
+import backgroundIMG from "../img/MoreAboutMe.png"
 
 function MoreAbout () {
     return (
         <>
         <Header />
-        <div id='MoreAboutMe' className="flex flex-col p-5">
-        <img className='mx-auto' id='selfPortrait' src={selfPortrait} alt="selfPortrait" />
-        <h1 className='mx-auto' id='moreH1'>More About Me</h1>
+        <div style={{ backgroundImage:`url(${backgroundIMG})`}} id='MoreAboutMe' className="flex flex-col p-5">
+        <img className='mx-auto mt-10' id='selfPortrait' src={selfPortrait} alt="selfPortrait" />
         <br />
+        <div id="aboutDiv">
+        <h1 className='mx-auto' id='moreH1'>More About Me</h1>
         <h3>Current Academic Plans - </h3>
-        <p className="mx-auto">I am a college student working to complete my Bachelors Degree in Computer Science. As I continue through college I am working on multiple Web-Development Projects and builds to keep my coding skills sharp! I am also planning on joining clubs next semester at school!</p>
+        <p className="mx-auto">I am a college student working towards completing my B.S. in Computer Science. As a part of my learning experience, I am partaking in multiple web-development projects and builds to hone my coding skills. Additionally, I will be joining clubs at school in the upcoming semester to further learn about computer science and gain new experiences.</p>
         <br />
         <h3> Personal Activities - </h3>
-        <p className="mx-auto">When I'm not at my desk working, I can be found in the gym or with my family. I started going to the gym over 28 months ago and I have not looked back since. I love everything about personal growth and just becoming the best version of myself. Family is at the forefront of everything, they always come first. I love talking to my family and I am blessed to have such a supportive group of people backing me up in everything I do!</p>
+        <p className="mx-auto">When I'm not at my desk working, I can be found spending quality time with my family and/or hitting the gym. I started going to the gym over two years ago and have since become passionate about taking ownership of my health and wellbeing. Going to the gym has not only been a great way to stay active, but it has also been an incredibly powerful and rewarding way to invest in my own self-growth. In addition to going to the gym, family has and always will be a priority to me. Aside from the immense love I get from my family, I am grateful for the support they offer to me in whatever I pursue.</p>
         <br />
         <h3>Future Plans- </h3>
-        <p className="mx-auto">My plans for the future are simple and as follows. I plan to keep my health as the forefront of everything I do. I plan to work hard to put myself in prosperous positions and open doors to great oppurtunities not just for me but for my family as well. Lastly, I plan to balance my work and personal life so I can be a smiling face to both my work family and actual family!</p>
+        <p className="mx-auto">My plans for the future include prioritizing my health, taking steps to establish myself professionally and nurturing meaningful relationships with loved ones while balancing my work and personal life. I would also really like to have a bunch of dogs someday! </p>
+        </div>
         </div>
         <Footer />
         </>
